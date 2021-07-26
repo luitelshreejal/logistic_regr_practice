d36d79adda9ad885fb592b582ca266c6ed548008
luitelshreejal <luitelshreejal@gmail.com>
2021-07-26 15:13:59 -0700

    Create logistic_regression_from_scratch.py
    First it calculates the log odds which signifies how many more times an event is likely to occur as compared to not occurring. The log odds are simply the dot product of the features (i.e. hours studied) and coefficients added with the intercept. This is passed into the sigmoid function (1/ 1+ e^-z) which calculates the probability. Now, if the probability is greater than the threshold then add 1 to the np array otherwise add 0. Return the np.array

---
+++ logistic_regression_from_scratch.py
@@ -0,0 +1,30 @@
+import numpy as np
+from exam import hours_studied, calculated_coefficients, intercept
+
+print(intercept)
+
+def log_odds(features, coefficients,intercept):
+  return np.dot(features,coefficients) + intercept
+
+def sigmoid(z):
+    denominator = 1 + np.exp(-z)
+    return 1/denominator
+
+# Create predict_class() function here
+
+def predict_class(features, coefficients, intercept, threshold):
+  calculated_log_odds = log_odds(features, coefficients, intercept)
+  probabilities = sigmoid(calculated_log_odds)
+  return np.where(probabilities >= threshold, 1, 0)
+  # array_with_threshold = []
+  # for i in probabilities:
+  #   if i >= threshold:
+  #     array_with_threshold.append(1)
+  #   elif i <= threshold:
+  #     array_with_threshold.append(0)
+
+
+
+final_results = predict_class(hours_studied, calculated_coefficients, intercept, 0.5)
+
+print(final_results
